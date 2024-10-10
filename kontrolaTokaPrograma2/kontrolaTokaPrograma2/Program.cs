94aade1bfb119098150c4fefe7af052e9fa31bae
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-10 15:13:38 +0200

    Add files via upload

---
+++ kontrolaTokaPrograma2/kontrolaTokaPrograma2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kontrolaTokaPrograma2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj:");
+            int broj = int.Parse(Console.ReadLine());
+
+            if (broj % 4 == 0 && broj % 6 == 0)
+            {
+                Console.WriteLine("Broj je djeljiv sa 4 i 6");
+            }
+
+            else if (broj % 6 == 0)
+            {
+                Console.WriteLine("Broj je djeljiv sa 6");
+            }
+
+            else if (broj % 4 == 0)
+            {
+                Console.WriteLine("Broj je djeljiv sa 4");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije djeljiv sa 4 ni sa 6");
+            }
+            Console.ReadKey();
+        }
+    }
+}
